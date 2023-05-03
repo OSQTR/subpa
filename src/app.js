86eb228e18d8894b9dd7e61b51de259c9d2ed979
OSQTR <clecat1005@gmail.com>
2023-05-03 16:52:46 +0900

    333

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
-let element = document.getElementsByClassName("profileAllow")[0];
+let element = document.getElementsByClassName("imgBanner")[0];
+let elementSec = document.getElementsByClassName("auth")[0];
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
   element.style.transform =
     "perspective(500px) rotateY(" + value * 0.01 + "deg)";
   let cnt = value * -0.05;
-  element.style.left = 30 - cnt * -1 + "px";
-  element.style.top = 30 - cnt * -1 + "px";
 
+  element.style.left = 20 - cnt * -1 + "px";
+  element.style.top = 0 - cnt * -1 + "px";
+
+  if (value > 200) {
+    valueSec = value - 200;
+    cnt = valueSec * -0.05;
+    elementSec.style.transform =
+      "perspective(500px) rotateY(" + valueSec * 0.01 + "deg)";
+    elementSec.style.left = 0 - cnt * -1 + "px";
+  }
   //스크롤 위치
   // console.log(value);
 
-  if (value > 200) {
+  if (value > 50) {
     document.getElementsByClassName("navBar")[0].style.display = "block";
   } else {
     document.getElementsByClassName("navBar")[0].style.display = "none";
